ba0aa2cc0c1004f726518a718986d5a49cdc7536
Moicano <jmariossilva@gmail.com>
2019-06-25 20:17:14 -0300

    RecipeList

---
+++ app/src/main/java/br/com/jmoicano/android/bakingapp/recipelist/RecipeListActivity.java
@@ -0,0 +1,24 @@
+package br.com.jmoicano.android.bakingapp.recipelist;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import br.com.jmoicano.android.bakingapp.R;
+
+public class RecipeListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_list);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
